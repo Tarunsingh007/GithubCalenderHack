6d68dd7f81e2cdaa7844e812d828b55af2293826
tarunsingh007 <tarunsingh233461@gmail.com>
2019-06-07 01:05:06 +0500

    c2

--- gitapp.py
+++ gitapp.py
@@ -2,21 +2,20 @@ api="e146e3c1e9413582394cdfd51f8975e8210db0d8"
 import moment
 import pprint
 import os
-from git import *
+import random
 
-
-filePath="./git.txt"
-date=moment.now()
-print(date)
-
-f=open("git.txt","a")
-f.write(str(date))
-f.close()
-
-# # for i in range(1):
-# os.system('cmd /c "git status"')
-# os.system('cmd /c "git add ."')
-# # os.system('cmd /c "git commit -m "c2""')
-# # os.system('cmd /c "git push origin master"')
-    
+for i in range(2):
+    x=random.randrange(0,54)
+    y=random.randrange(0,6)
+    date=moment.now().subtract(years=1).add(days=1).add(weeks=x).add(days=y)
+    print(date)
+    filePath="./git.txt"
+    f=open("git.txt","a")
+    f.write(str(date))
+    f.close()
+    os.system('cmd /c "git status"')
+    os.system('cmd /c "git add ."')
+    os.system('cmd /c "git commit -m "c2" --date={}"'.format(date))
+    os.system('cmd /c "git push origin master"')
+        
     
